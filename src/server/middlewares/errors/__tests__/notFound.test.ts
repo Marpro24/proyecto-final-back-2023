69c75eca263bd626b15d211b0744268210a12526
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-28 14:03:34 +0100

    Add notFound integration test

--- src/server/middlewares/errors/__tests__/notFound.test.ts
+++ src/server/middlewares/errors/__tests__/notFound.test.ts
@@ -1,22 +1,19 @@
-import { type Request, type Response, type NextFunction } from "express";
-import type CustomError from "../../../CustomError/CustomError";
-import { notFound } from "../generalError";
+import request from "supertest";
+import app from "../../../app";
+import "../../../../server/index.js";
 
-describe("Given a notFound middleware method", () => {
-  describe("When it receives a response", () => {
-    test("Then it should call the next function with a 404 message and an 'Endpoint not found'", () => {
-      const req = {};
-      const res = {};
-      const next = jest.fn();
+describe("Given a GET /gallery endpoint", () => {
+  describe("When it receives a request", () => {
+    test("Then it should respond with a 404 and an 'Endpoint not found' message", async () => {
+      const expectedSatus = 404;
+      const expectedMessage = "Endpoint not found";
+      const requestedPath = "/gallery";
 
-      const customError: Partial<CustomError> = {
-        message: "Endpoint not found",
-        statusCode: 404,
-      };
+      const response = await request(app)
+        .get(requestedPath)
+        .expect(expectedSatus);
 
-      notFound(req as Request, res as Response, next as NextFunction);
-
-      expect(next).toHaveBeenCalledWith(expect.objectContaining(customError));
+      expect(response.body).toHaveProperty("error", expectedMessage);
     });
   });
 });
