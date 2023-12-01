e43a7bf82dca794ea168422d92891e94f90c47bf
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-12-01 13:38:28 +0100

    Add postman endpoints collection

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,4 +4,10 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.ts"],
   resolver: "jest-ts-webcompat-resolver",
+  collectCoverageFrom: [
+    "**/*.ts",
+    "!src/index.ts",
+    "!src/server/app.ts",
+    "!src/database/index.ts",
+  ],
 };
