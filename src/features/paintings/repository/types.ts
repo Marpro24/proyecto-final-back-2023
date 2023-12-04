611c19cb713f694ab5714bd1987f6ac978601f57
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-12-05 00:21:32 +0100

    Add delete painting repository

--- src/features/paintings/repository/types.ts
+++ src/features/paintings/repository/types.ts
@@ -2,4 +2,5 @@ import { type PaintingStructure } from "../types";
 
 export interface PaintingsRepository {
   getPaintings: () => Promise<PaintingStructure[]>;
+  deletePainting: (paintingId: string) => Promise<void>;
 }
